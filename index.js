819e0143aab4efb8c86b0e6bbe896f56bdbffe5a
Naim <hossainnaeem299@gmail.com>
2026-02-12 04:35:21 +0600

    Third commit

--- index.js
+++ index.js
@@ -5,5 +5,9 @@ function sayName(){
     console.log('My name is ' + name1)
 }
 function isAbove(){
-    return age >=18
+    return age >17
+}
+
+function newFn(){
+     console.log('It is new function')
 }
