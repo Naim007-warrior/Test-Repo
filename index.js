2c30ed720ae0ce75ecaefabcfb0d6fa74d55a59f
Naim <hossainnaeem299@gmail.com>
2026-02-11 14:03:46 +0600

    Added isAbove18 function

--- index.js
+++ index.js
@@ -2,5 +2,8 @@ const age =23;
 const name = 'Naim'
 
 function sayName(){
-    console.log('My name is ' + name)
+    console.log('My name is ' + name1)
+}
+function isAbove(){
+    return age >=18
 }
